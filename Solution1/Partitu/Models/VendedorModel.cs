2cc747b0a9b1b080181a4a627604e56e78909c96
Almeifrancisco <almeifrancisco@gmail.com>
2015-05-27 10:25:56 -0300

    back commit

---
+++ Solution1/Partitu/Models/VendedorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Partitu.Models
+{
+    public class VendedorModel : UsuarioModel
+    {
+
+        public string descricao { get; set; }
+        public string  foto { get; set; }
+        public File file { get; set; }
+        public string url { get; set; }
+    }
+}
---
+++ Solution1/Partitu/Scripts/edi.js
@@ -0,0 +1,8 @@
+$(document).ready(function(e) {
+	
+	
+
+
+});
+
+function check() { $("#vendedor"). }  
